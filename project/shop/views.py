40adac48ec80c4fb5b3e6bff97842f7a7389bdd0
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-06-06 23:48:57 +0300

    updated

--- project/shop/views.py
+++ project/shop/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+# from django.template.response import TemplateResponse
 
 
 def index(request):
     return render(request, 'templ/base.html')
+
+# def index(request):
+#    return TemplateResponse(request,  "shop/base.html")
+
