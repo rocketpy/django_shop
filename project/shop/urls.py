fe417be867e9e296461dbae8d7217c1e14c2ec6b
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-06-07 18:55:01 +0300

    updated

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home')
-]
+               path('', views.index, name='home'),
+               path('about/', views.about, name='about'),
+               path('services/', views.services, name='services'),
+               path('contacts/', views.contacts, name='contacts'),
+               path('item/<int:pk>/', views.items, name='items')
+               ]
--- project/shop/views.py
+++ project/shop/views.py
@@ -5,6 +5,22 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'templ/base.html')
 
+
+def about(request):
+    return render(request, 'templ/about.html')
+
+
+def services(request):
+    return render(request, 'templ/services.html')
+
+
+def contacts(request):
+    return render(request, 'templ/contacts.html')
+
+
+def items(request):
+    return render(request, 'templ/item.html')
+
 # def index(request):
 #    return TemplateResponse(request,  "shop/base.html")
 
