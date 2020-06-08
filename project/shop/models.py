69a71dc25c9aa3d6a3fc1a47f17864e6c597ba66
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-06-08 23:53:36 +0300

    updated

--- project/shop/models.py
+++ project/shop/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 
-
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
 
 
 #  for update use:  python manage.py makemigrations blog_app
