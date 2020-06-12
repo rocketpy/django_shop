f2c195f7b845ab01b481d8e11015bf2717fac985
rocketpy <dimitr.ushakov@gmail.com>
2020-06-12 23:46:54 +0300

    updated

--- project/shop/models.py
+++ project/shop/models.py
@@ -6,6 +6,18 @@ class Item(models.Model):
     prod_descrip = models.TextField()
     price = models.IntegerField()
     img_url = models.ImageField()
+    
+    
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '%s' % self.name
+
+    class Meta:
+        verbose_name = 'Product category'
+        verbose_name_plural = 'Categories'
 
 
 #  for update use:  python manage.py makemigrations shop
