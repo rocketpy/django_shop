e7ef009e00cc0d5367361e30befbc58b48d55d65
rocketpy <dimitr.ushakov@gmail.com>
2020-06-16 23:36:15 +0300

    updated

--- project/shop/models.py
+++ project/shop/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.name
+
+
 class Item(models.Model):
     prod_name = models.CharField(max_length=50)
     prod_descrip = models.TextField()
