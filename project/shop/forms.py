ef9a4b1cf4eb1de1c4c9ee448674353488c3a26b
rocketpy <dimitr.ushakov@gmail.com>
2020-06-16 23:50:53 +0300

    updated

--- project/shop/forms.py
+++ project/shop/forms.py
@@ -5,4 +5,6 @@ from models import *
 class CustomerForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
-    
+        class Meta:
+        model = Customer
+        fields = ('first_name', 'last_name', 'email', 'password')
