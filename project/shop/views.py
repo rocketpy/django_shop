1e9b3474a927855df323a5b45c26498e3a148ea5
rocketpy <dimitr.ushakov@gmail.com>
2020-06-22 00:04:58 +0300

    modified

--- project/shop/views.py
+++ project/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from . import *
+from .models import *
 
 
 def index(request):
