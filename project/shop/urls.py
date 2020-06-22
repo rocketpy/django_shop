3ff602cb3ebf245327ca75b3ae5ba898def51a57
rocketpy <dimitr.ushakov@gmail.com>
2020-06-22 23:05:07 +0300

    updated

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -7,12 +7,11 @@ urlpatterns = [
                path('about/', views.about, name='about'),
                path('services/', views.services, name='services'),
                path('contacts/', views.contacts, name='contacts'),
-               path('item/<int:pk>/', views.items, name='items'),
                path('notebooks/', views.notebooks, name='notebooks'),
                path('smartphones/', views.smartphones, name='smartphones'),
                path('tablets/', views.tablets, name='tablets'),
                path('cart/', views.cart, name='cart'),
-               path(r'^product/(?P<product_id>\w+)/$', views.product, name='product'),
+               path('product/<int:pk>/', views.product, name='product'),
                path('login/', views.login_page, name='login'),
                path('register/', views.register_page, name='register')
                ]
