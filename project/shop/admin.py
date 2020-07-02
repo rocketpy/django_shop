dbe0d51a17505ee8b1c25f34eb9fed1e1ec33559
rocketpy <dimitr.ushakov@gmail.com>
2020-07-03 00:03:09 +0300

    updated

--- project/shop/admin.py
+++ project/shop/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from .models import Customer, ProductCategory, Product, CartItem, ShoppingCart
 
 
-admin.site.register()
-admin.site.register()
-admin.site.register()
-admin.site.register()
-admin.site.register()
+admin.site.register(Customer)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(ShoppingCart)
 
