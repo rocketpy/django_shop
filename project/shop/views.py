d0ac15e34183df8c572d97156eec6cda2c9a0bcf
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-07-02 23:41:25 +0300

    updated

--- project/shop/views.py
+++ project/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from django.contrib import messages
